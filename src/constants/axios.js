452e3569a9633de977f15e100a907c4c828bea32
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-05-06 00:11:59 +0200

    updated axios baseURL

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://68x0x5mqsh.execute-api.us-east-1.amazonaws.com/Prod",
     timeout: 30000,
     headers: {
         "Content-Type": "application/json",
