4adb9dd5992d5a45efd27cf22b3ce4f75f1a0780
AmaySunilShenoy <amaysunilshenoy02@gmail.com>
2024-05-06 02:40:48 +0200

    fix body strucutre api

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -5,8 +5,6 @@ export const instance = axios.create({
     timeout: 30000,
     headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": "*"
     },
 });
